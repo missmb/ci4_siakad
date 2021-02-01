dd352775941be6da5b9f65e2328cf7d8809c3237
Mar'atul Bariroh <maratulbariroh3630@gmail.com>
2021-02-01 15:58:06 +0700

    table faculty

---
+++ app/Views/Admin/faculty.php
@@ -0,0 +1,33 @@
+<div class="row">
+  <div class="col-sm-12">
+    <div class="card card-success">
+      <div class="card-header">
+        <h3 class="card-title">Data <?= $title; ?></h3>
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool"><i class="fas fa-plus"></i> Add
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        <table id="example1" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th width="20px">No</th>
+              <th>Faculty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+  </div>
+</div>
--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -16,36 +16,3 @@
   </div> -->
 </div>
 
-<div class="row">
-  <div class="col-sm-12">
-    <div class="card card-success">
-      <div class="card-header">
-        <h3 class="card-title">Data <?= $title; ?></h3>
-
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool"><i class="fas fa-plus"></i> Add
-          </button>
-        </div>
-      </div>
-      <div class="card-body">
-        <table class="table table-bordered table-responsive" id="example2">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>Faculty</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!-- /.card-body -->
-    </div>
-  </div>
-</div>
